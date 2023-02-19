e510fdb0b74dd0575543d6e325f250340bd9afce
Jordan Wright <wright.jordan.n@outlook.com>
2023-02-18 18:55:49 -0700

    go back :(

--- src/index.ts
+++ src/index.ts
@@ -24,15 +24,12 @@ export class MemoryStore implements SessionStore {
       return { data: null, err: null };
     }
     const now = Math.floor(Date.now() / 1000);
-    if (now > data.absoluteDeadline) {
+    if (now > data.idleDeadline || now > data.absoluteDeadline) {
       this.#m.delete(id);
       return { data: null, err: null };
     }
     data.idleDeadline = now + ttl;
     this.#m.set(id, data);
-    setTimeout(async () => {
-      this.#m.delete(id);
-    }, ttl * 1000);
     return { data, err: null };
   }
   async set(
